9c0a639cf64a2aa3e740bc8db38e5b4ddf5d38a4
leandroReal2025 <contato.leandrodigitalon@gmail.com>
2025-07-15 12:09:22 -0300

    Pagina sobre

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const cont_prod = document.getElementById('container-produtos');
 
 const produtos = [
     {
-        img: "./img/prog.png",
+        img: "../img/prog.png",
         nome: "Progressiva Vegetal Havana - 500 ML",
         desc: "Alinha os fios, reduz o frizz e proporciona brilho intenso com efeito duradouro",
         link: "https://entrega.logzz.com.br/pay/mem37nr45/progcreme150"
     },
     {
-        img: "./img/relogioD20.png",
+        img: "../img/relogioD20.png",
         nome: "Novo Smartwatch D20",
-        desc: "Um dispositivo inteligente que combina, estilo tecnologia e saúde, com alta definição você pode visualizar suas notificações, chamadas e mensagens com clareza incrível mesmo sob a luz do sul",
+        desc: "um dispositivo inteligente que combina, estilo tecnologia e saúde, com alta definição você pode visualizar suas notificações, chamadas e mensagens com clareza incrível mesmo sob a luz do sul",
         link: "https://entrega.logzz.com.br/pay/mem5mn487/1-unidade-relogio-smart-oferta-4"
     }
 ];
 
 produtos.forEach((element) => {
     let novoLink = document.createElement("a");
-    novoLink.target = "_blank";
+    novoLink.target = "blank";
     novoLink.classList.add("container-links");
 
     novoLink.innerHTML = `
